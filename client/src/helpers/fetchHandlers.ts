a20b5094d45647eb82822fa13aa415dab32862ae
Léonard Wojcik <leonardw@tutanota.com>
2022-11-03 16:36:12 +0100

    feat: add form to create blog post

---
+++ client/src/helpers/fetchHandlers.ts
@@ -0,0 +1,22 @@
+/**
+ * Fetches data from url.
+ * @param {string} apiEndpoint - Fetched url
+ * @param requestBody  - body send with POST method
+ * @returns {}
+ */
+export const genericPostRequest = async (
+    apiEndpoint: string,
+    requestBody: any
+) => {
+    try {
+        const response = await fetch(apiEndpoint, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(requestBody),
+        });
+        const data = await response.json();
+        return { data };
+    } catch {
+        return false;
+    }
+};
--- client/src/pages/Blog/Blog.tsx
+++ client/src/pages/Blog/Blog.tsx
@@ -1,10 +1,59 @@
+// React Hooks
+import { useRef } from 'react';
+
 // Page components
 import BlogPost from '../../components/BlogPost/BlogPost';
+import { genericPostRequest } from '../../helpers/fetchHandlers';
+
+// CSS
+import classes from './Blog.module.css';
 
 function Blog() {
+    // Refs
+    const postTitleRef = useRef<HTMLInputElement>(null);
+    const postContentRef = useRef<HTMLTextAreaElement>(null);
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const enteredTitle = postTitleRef.current?.value;
+        const enteredContent = postContentRef.current?.value;
+
+        const formData = {
+            title: enteredTitle,
+            content: enteredContent,
+        };
+
+        const requestResponse: any = await genericPostRequest(
+            'http://localhost:5000/posts/',
+            formData
+        );
+
+        console.log(requestResponse);
+    };
+
     return (
-        <div>
+        <div className={classes['blog-page']}>
             <h1>Blog page</h1>
+            <section>
+                <h2>Add a blog post</h2>
+                <form
+                    onSubmit={handleSubmit}
+                    className={classes['new-blog-post-form']}
+                >
+                    <label htmlFor="post-title">Post title</label>
+                    <input ref={postTitleRef} type="text" id="post-title" />
+
+                    <label htmlFor="post-content">Post content</label>
+                    <textarea ref={postContentRef} id="post-content"></textarea>
+
+                    <button>Add blog post</button>
+                </form>
+            </section>
+
+            <section>
+                <h2>Your blog posts</h2>
+            </section>
             <BlogPost />
         </div>
     );
