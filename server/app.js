8019270f34a8933e43e169f3e7fb754b2a4c9084
Léonard Wojcik <leonardw@tutanota.com>
2022-11-07 11:34:40 +0100

    build(server): setup code format script

--- server/app.js
+++ server/app.js
@@ -3,16 +3,15 @@ const cors = require('cors');
 
 const { query } = require('express');
 
-const postRoutes = require('./routes/posts.routes')
+const postRoutes = require('./routes/posts.routes');
 
 const app = express();
 
 // Middleware
-app.use(cors())
-app.use(express.json()) // Allows to access req.body;
+app.use(cors());
+app.use(express.json()); // Allows to access req.body;
 app.use('/posts', postRoutes);
-
 
 app.listen(5000, () => {
     console.log('Listening for requests on port 5000');
-})
+});
--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -8,37 +8,39 @@ const router = require('express').Router();
 router.post('/', async (req, res) => {
     try {
         const { title, content } = req.body;
-        const newBlogPost = await pool.query("INSERT INTO blog_posts (title, content) VALUES($1, $2) RETURNING *", [title, content]);
+        const newBlogPost = await pool.query(
+            'INSERT INTO blog_posts (title, content) VALUES($1, $2) RETURNING *',
+            [title, content]
+        );
         res.json(newBlogPost.rows[0]);
-
     } catch (err) {
-        console.error(err.message)
-
+        console.error(err.message);
     }
-
-})
+});
 
 // Select all blog posts
 router.get('/', async (req, res) => {
     try {
-        const allBlogPosts = await pool.query("SELECT * FROM blog_posts");
-        res.json(allBlogPosts.rows)
+        const allBlogPosts = await pool.query('SELECT * FROM blog_posts');
+        res.json(allBlogPosts.rows);
     } catch (err) {
-        console.error(err.message)
+        console.error(err.message);
     }
-})
+});
 
 // Select a blog post by id
 router.get('/:id', async (req, res) => {
     try {
         const { id } = req.params;
-        const blogPost = await pool.query("SELECT * FROM blog_posts WHERE id = $1", [id]);
+        const blogPost = await pool.query(
+            'SELECT * FROM blog_posts WHERE id = $1',
+            [id]
+        );
         res.json(blogPost.rows[0]);
-
     } catch (err) {
-        console.error(error.message)
+        console.error(error.message);
     }
-})
+});
 
 // Edit a blog post
 router.put('/:id', async (req, res) => {
@@ -46,22 +48,27 @@ router.put('/:id', async (req, res) => {
         const { id } = req.params;
         const { title } = req.body;
         // Todo : set title AND content of the blog
-        const updateBlogPost = await pool.query("UPDATE blog_posts SET title = $1 WHERE id = $2", [title, id]);
-        res.json("Blog post has been updated successfully.");
-
+        const updateBlogPost = await pool.query(
+            'UPDATE blog_posts SET title = $1 WHERE id = $2',
+            [title, id]
+        );
+        res.json('Blog post has been updated successfully.');
     } catch (err) {
         console.error(err.message);
     }
-})
+});
 // Delete a blog post
 router.delete('/:id', async (req, res) => {
     try {
         const { id } = req.params;
-        const deleteBlogPost = await pool.query('DELETE FROM blog_posts WHERE id = $1', [id]);
-        res.json("Blog post has been deleted successfully.");
+        const deleteBlogPost = await pool.query(
+            'DELETE FROM blog_posts WHERE id = $1',
+            [id]
+        );
+        res.json('Blog post has been deleted successfully.');
     } catch (err) {
         console.error(err.message);
     }
-})
+});
 
 module.exports = router;
